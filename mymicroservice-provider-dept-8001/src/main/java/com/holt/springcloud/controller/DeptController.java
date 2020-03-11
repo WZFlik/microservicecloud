57fcad33fd4f1243667bf03370a779edffd246b2
侯天浩 <hth146@163.com>
2020-03-11 17:23:38 +0800

    修改DeptController

--- mymicroservice-provider-dept-8001/src/main/java/com/holt/springcloud/controller/DeptController.java
+++ mymicroservice-provider-dept-8001/src/main/java/com/holt/springcloud/controller/DeptController.java
@@ -20,7 +20,7 @@ import com.holt.springcloud.service.DeptService;
  */
 @RestController
 public class DeptController {
-//      注入入DeptService
+//注入入DeptService
 	@Autowired
 	private DeptService service;
 
