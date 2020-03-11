635ae3d4f07afa99a23f7db4657e5cf53568a4dc
侯天浩 <hth146@163.com>
2020-03-11 17:06:54 +0800

    修改Service 忽略.ignore和Classpath文件

--- mymicroservice-provider-dept-8001/src/main/java/com/holt/springcloud/service/DeptService.java
+++ mymicroservice-provider-dept-8001/src/main/java/com/holt/springcloud/service/DeptService.java
@@ -6,8 +6,11 @@ package com.holt.springcloud.service;
 import java.util.List;
 
 import com.holt.spirngcloud.entities.Dept;
-public interface DeptService
-{
+/**
+ * @author hot
+ *Deptservice接口
+ */
+public interface DeptService{
 public boolean add(Dept dept);
 public Dept    get(Long id);
 public List<Dept> list();
