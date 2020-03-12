0949405667c2d246d96635a24b971d96949fb982
侯天浩 <hth146@163.com>
2020-03-12 13:44:11 +0800

    新建Eureka服务端，并配置

--- mymicroservice-provider-dept-8001/src/main/java/com/holt/springcloud/DeptProvider8001_APP.java
+++ mymicroservice-provider-dept-8001/src/main/java/com/holt/springcloud/DeptProvider8001_APP.java
@@ -2,6 +2,7 @@ package com.holt.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  *@author hot
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *this is run APP
  */
 @SpringBootApplication
+@EnableEurekaClient  //zureka的客户端
 public class DeptProvider8001_APP {
 	public static void main(String[] args) {
 		SpringApplication.run(DeptProvider8001_APP.class, args);
