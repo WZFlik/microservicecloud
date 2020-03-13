22a0afcdedf2b197e83bb03dd4f6b76778efd582
侯天浩 <hth146@163.com>
2020-03-13 13:22:23 +0800

    添加@FeignCliens 组件扫描 在Controller中引入FeignService

---
+++ mymicroservice-customer-dept-feign/src/main/java/com/holt/springcloud/DeptConsumer_feign_APP.java
@@ -0,0 +1,23 @@
+package com.holt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ *@author hot
+ *@since 2020年3月11日
+ *@version     
+ *对外暴漏的消费者的启动类
+ */
+@SpringBootApplication
+@EnableEurekaClient        //eureka客户端
+@EnableFeignClients(basePackages = {"com.holt.spirngcloud.feignservice"})
+public class DeptConsumer_feign_APP {
+	public static void main(String[] args) {
+		SpringApplication.run(DeptConsumer_feign_APP.class, args);
+	}
+
+}
---
+++ mymicroservice-customer-dept-feign/src/main/java/com/holt/springcloud/controller/DeptController_Feign_Consumer.java
@@ -0,0 +1,65 @@
+package com.holt.springcloud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.holt.spirngcloud.entities.Dept;
+import com.holt.spirngcloud.feignservice.DeptFeignService;
+
+/**
+ *@author hot
+ *@since 2020年3月11日
+ *@version     
+ * 对外暴漏的消费者的controller层
+ */
+@RestController
+public class DeptController_Feign_Consumer {
+	
+	@Autowired
+	private DeptFeignService feignService ;
+	
+	/**
+	 * 使用restTeplate 连接DeptController的url
+	 * @param dept
+	 * @return
+	 */
+	@RequestMapping("/consumer/dept/add")
+	public Boolean addDept(Dept dept) {
+		return feignService.add(dept);
+	}
+	
+	/**
+	 *  根据id获取Dept
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/consumer/dept/get/{id}")
+	public Dept getDept(@PathVariable(value = "id") Long id) {
+		return feignService.get(id);
+
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	@GetMapping("/consumer/dept/list")
+	public List<Dept> getList() {
+		System.out.println("*********Feign---list");
+		return feignService.list();
+
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
